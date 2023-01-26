f83c4612d5a1e0088e5c04ee88e871e61d046fc6
Syscore1 <32805386+Syscore1@users.noreply.github.com>
2023-01-25 19:46:38 -0700

    Update run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-sudo yum update -y > /dev/null 2> /dev/null < /dev/null &
 cd /
 cd var/www/html/ECESaaSUACJ
 sudo rm .env
@@ -7,8 +6,8 @@ echo removed
 sudo aws s3 cp s3://s3intercambio2/ECESaaSUACJ/.env /var/www/html/ECESaaSUACJ/
 sudo chown -R ec2-user ./composer.lock
 echo composerpreview
-sudo -u ec2-user composer update > composer.txt  > /dev/null 2> /dev/null < /dev/null &
-php artisan migrate --force > migrate.txt > /dev/null 2> /dev/null < /dev/null &
+sudo -u ec2-user composer update > composer.txt 
+php artisan migrate --force > migrate.txt
 aws s3 cp composer.txt s3://s3intercambio2/ECESaaSUACJ/logs/
 aws s3 cp migrate.txt s3://s3intercambio2/ECESaaSUACJ/logs/
 echo artisan
