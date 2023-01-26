8976db6373ba6b01c1b0553f743d2024f11b3182
Syscore1 <32805386+Syscore1@users.noreply.github.com>
2023-01-25 18:10:44 -0700

    Update run.sh

--- tests/run.sh
+++ tests/run.sh
@@ -4,7 +4,7 @@ cd /
 cd var/www/html/ECESaaSUACJ
 sudo rm .env
 echo removed 
-sudo aws s3 cp .env /var/www/html/ECESaaSUACJ/
+sudo aws s3 cp s3://s3intercambio2/ECESaaSUACJ/.env /var/www/html/ECESaaSUACJ/
 sudo chown -R ec2-user ./composer.lock
 echo composerpreview
 sudo -u ec2-user composer update > composer.txt  > /dev/null 2> /dev/null < /dev/null &
